238d0b06b8ddfdb565d213b29e761aad3fd184d0
Phantomfall <smileysteven1@gmail.com>
2018-03-12 23:10:12 -0400

    Week 6 Final Prject

--- week_6/lam_Steven/script.js
+++ week_6/lam_Steven/script.js
@@ -2,35 +2,35 @@ console.log(google)
 
 google.maps.event.addDomListener(window, 'load', function() {
      var colors = ['Blue and Yellow', 'Red and White', 'Red and White', 'Blue White and Red'];
-	var wonders = [
+	var faveCities = [
 		{
-			name: '<h2>Stockholm</h2>',
+			name: '<h4>Stockholm</h4>',
 			description: '',
-		image: '<img height="100" src="https://vignette.wikia.nocookie.net/pvx/images/a/a4/Sweden-flag.jpg/revision/latest?cb=20080611175424">',
+		image: '<img height="75" src="https://vignette.wikia.nocookie.net/pvx/images/a/a4/Sweden-flag.jpg/revision/latest?cb=20080611175424">',
 			lat: '59.3293',
 			lng: '18.0686',
             color: '<br><br><b>National colours:</b> '+colors[0],
 		},
 		{
-			name: '<h2>Paris</h2>',
+			name: '<h4>Paris</h4>',
 //			description: '<p>The <b>Hanging Gardens of Babylon</b> were one of the <a target="_blank" href="http://en.wikipedia.org/wiki/Seven_Wonders_of_the_Ancient_World" title="Seven Wonders of the Ancient World">Seven Wonders of the Ancient World</a>, and the only one whose location has not been definitely established.</p>',
-		image: '<img height="100" src="https://www.graphicmaps.com/r/w1047/images/flags/fr-flag.jpg">',
+		image: '<img height="75" src="https://www.graphicmaps.com/r/w1047/images/flags/fr-flag.jpg">',
 			lat: '48.8566',
 			lng: '2.3522',
              color: '<br><br><b>National colours:</b> '+colors[3],
 		},
         {
-			name: '<h2>Toronto</h2>',
+			name: '<h4>Toronto</h4>',
 //			description: '<p>The <b>Hanging Gardens of Babylon</b> were one of the <a target="_blank" href="http://en.wikipedia.org/wiki/Seven_Wonders_of_the_Ancient_World" title="Seven Wonders of the Ancient World">Seven Wonders of the Ancient World</a>, and the only one whose location has not been definitely established.</p>',
-		image: '<img height="100" src="https://vignette.wikia.nocookie.net/canada/images/d/d4/Canadian_Flag.jpg/revision/latest/scale-to-width-down/640?cb=20100820020541">',
+		image: '<img height="75" src="https://vignette.wikia.nocookie.net/canada/images/d/d4/Canadian_Flag.jpg/revision/latest/scale-to-width-down/640?cb=20100820020541">',
 			lat: '43.6532',
 			lng: '-79.3832',
              color: '<br><br><b>National colours:</b> '+colors[1],
 		},
           {
-			name: '<h2>Copenhagan</h2>',
+			name: '<h4>Copenhagan</h4>',
 //			description: '<p>The <b>Hanging Gardens of Babylon</b> were one of the <a target="_blank" href="http://en.wikipedia.org/wiki/Seven_Wonders_of_the_Ancient_World" title="Seven Wonders of the Ancient World">Seven Wonders of the Ancient World</a>, and the only one whose location has not been definitely established.</p>',
-			image: '<img height="100" src="https://vignette.wikia.nocookie.net/harrypotter/images/d/d8/Flag_of_Denmark.jpg/revision/latest?cb=20091208000818">',
+			image: '<img height="75" src="https://vignette.wikia.nocookie.net/harrypotter/images/d/d8/Flag_of_Denmark.jpg/revision/latest?cb=20091208000818">',
 			lat: '55.6761',
 			lng: '12.5683',
                color: '<br><br><b>National colours:</b> '+colors[1],
@@ -39,13 +39,13 @@ google.maps.event.addDomListener(window, 'load', function() {
 	];
     
    
-	var latlng = new google.maps.LatLng('40', '15');
+	var latlng = new google.maps.LatLng('50', '340');
 	var mapOptions = {
 		center: latlng,
-		zoom: 3,
+		zoom: 2,
 	};
-	var map = new google.maps.Map(document.getElementById('wonders-map'), mapOptions);
-	wonders.forEach(function(wonder) {
+	var map = new google.maps.Map(document.getElementById('geo-map'), mapOptions);
+	faveCities.forEach(function(wonder) {
 		var latlng = new google.maps.LatLng(wonder.lat, wonder.lng);
 		var marker = new google.maps.Marker({
 			position: latlng,
